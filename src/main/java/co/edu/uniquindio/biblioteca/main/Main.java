54ad846424dcda885a72b01d52b3ae01b415cf37
AndresCepedaC <143088681+AndresCepedaC@users.noreply.github.com>
2024-02-22 23:24:04 -0500

    cree 2 metodos
    creacion de 2 metodos que se relacionen con 2 o mas clases

--- src/main/java/co/edu/uniquindio/biblioteca/main/Main.java
+++ src/main/java/co/edu/uniquindio/biblioteca/main/Main.java
@@ -2,31 +2,60 @@ package co.edu.uniquindio.biblioteca.main;
 
 import co.edu.uniquindio.biblioteca.*;
 
+import java.net.IDN;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args){
+        Biblioteca biblioteca = inicializarDatosPrueba();
+
+        encontrarPrestamoMiembro(biblioteca,"1234");
+        imprimirItems(biblioteca.getListaItems());
+    }
+    public static Biblioteca inicializarDatosPrueba(){
         Biblioteca biblioteca = new Biblioteca();
         biblioteca.setNombre("Bliblioteca UQ");
         SimpleDateFormat sdf = new SimpleDateFormat();
-        Prestamo prestamo = null;
-        Libro libro = new Libro("Andres","123",true, prestamo, "Jorge");
-        Dvd dvd = new Dvd("jorge el curioso", "321", true, prestamo, sdf.parse("09/12/2023"));
-        Revista revista = new Revista("Ivone", "234", false, prestamo, "volumen 3");
-        Miembro miembro = new Miembro("Ivan", "1234", (List<Prestamo>) prestamo);
-        prestamo = new Prestamo(libro, miembro, sdf.parse("20/01/2024"), sdf.parse("30/12/2023"));
+        Prestamo prestamo = new Prestamo();
+        Libro libro = new Libro("Andres","123",true, "Jorge");
+        Dvd dvd = new Dvd("jorge el curioso", "321", true, LocalDate.of(2021,10,20));
+        Revista revista = new Revista("Ivone", "234", false, "volumen 3");
+        Miembro miembro = new Miembro("Ivan", "1234");
+        prestamo = new Prestamo(libro, miembro, LocalDate.of(2024,02,01), LocalDate.of(2023, 01, 30));
+        libro.setPrestamo(prestamo);miembro.getListaPrestamos().add(prestamo);
         Bibliotecario bibliotecario = new Bibliotecario("Raul", "12345");
         Multa multa = new Multa();
-        biblioteca.getListaItems().add(dvd);biblioteca.getListaItems().add(libro);biblioteca.getListaItems().add(revista);
+        biblioteca.getListaItems().add(dvd);biblioteca.getListaItems().add(libro);biblioteca.getListaItems().add(revista);biblioteca.getListaMiembros().add(miembro);
         biblioteca.getListaPrestamos().add(prestamo);biblioteca.getListaEmpleados().add(bibliotecario);biblioteca.getListaMultas().add(multa);
-        imprimirItems(biblioteca.getListaItems());
+        return biblioteca;
     }
     public static void imprimirItems(List<Item> listaItems){
-        Biblioteca biblioteca = new Biblioteca();
-        for (Item item : biblioteca.getListaItems()){
+        for (Item item : listaItems){
             System.out.println("Item: " + item.toString());
         }
     }
+    public static void encontrarPrestamoMiembro(Biblioteca biblioteca, String ID){
+        for (Miembro miembro: biblioteca.getListaMiembros()){
+            if (miembro.getID().equalsIgnoreCase(ID)){
+                for (Prestamo prestamo : miembro.getListaPrestamos()){
+                    System.out.println(prestamo.toString());
+                }
+            }else{
+                System.out.println("No existe el miembro.");
+            }
+        }
+    }
+    public static void buscarMiembroAsociado(Biblioteca biblioteca, String ISBN){
+        for (Item item : biblioteca.getListaItems()){
+            if (item.getISBN().equalsIgnoreCase(ISBN)){
+                System.out.println(item.getPrestamo().getMiembro().toString());
+            }else{
+                System.out.println("No se encuentra el item.");
+            }
+        }
+    }
+
 }
